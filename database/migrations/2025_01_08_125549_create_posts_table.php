c7bf4e2fb4279e55c16d61aa5c45bf46f3763620
orun-sunny <sunny.mukut@gmail.com>
2025-01-09 12:31:38 +0600

    gg

--- database/migrations/2025_01_08_125549_create_posts_table.php
+++ database/migrations/2025_01_08_125549_create_posts_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('description');
+            $table->string('image');
             $table->timestamps();
         });
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
+    <script
+        src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+
+    <style type="text/tailwindcss">
+        @layer utilities {
+            .container {
+                @apply px-10 mx-auto;
+            }
+
+
+        }
+
+
+    </style>
 </head>
-<body><p>
-    welsome in my wbesit
-</p>
+<body>
+<div class="container">
+    <div class="flex justify-between">
+        <h1>Hello World</h1>
+        <a href="/create" class="bg-green-600 text-white-rounded p-4">Crud new Project</a>
+    </div>
+</div>
 </body>
 </html>
+
